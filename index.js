1778794ffad769e1adf6a6d43ca6f7b158181246
NASA12345 <nayan11404@gmail.com>
2019-12-29 18:15:51 +0530

    [GCI-<Nayan>] message

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/info', (req, res) => res.send("{ serverName: 'google2', serverVersion: '0.1.0' }"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
